089a4415c02dfbeae0637099363256b9929e71be
Bauyrzhan Kikanov <baurkika@gmail.com>
2020-08-06 18:05:26 -0400

    Updated the  Solution

--- cc-001-calculate-gasoline-production-cost/calculateGasolineCost.py
+++ cc-001-calculate-gasoline-production-cost/calculateGasolineCost.py
@@ -1,8 +1,8 @@
 gasGallon = float(input("Enter a number which stands for gallons of gasoline: "))
 gasLiter = gasGallon * 3.7854
 barrel = gasGallon / 19.5
-cost = gasLiter * 0.75
+costPerLiter = gasLiter * 0.75
 
 print("{} gallon(s) = {} liter(s).".format(gasGallon, gasLiter))
 print("{} barrel(s) of oil required to produce {} gallon(s) of gas".format(barrel,gasGallon))
-print("It will cost {} dollar(s)".format(cost))
+print("It will cost {} dollar(s)".format(costPerLiter))
